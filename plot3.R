5166200b0c97470bdffed9ff9f9a56e73407b2f1
Marco Tizzoni <marco.tizzoni@gmail.com>
2016-01-21 17:11:52 +0100

    Fix axis labels

---
+++ plot3.R
@@ -0,0 +1,30 @@
+source("common.R")
+
+# Of the four types of sources indicated by the (point, nonpoint, onroad,
+# nonroad) variable, which of these four sources have seen decreases in
+# emissions from 1999-2008 for Baltimore City? Which have seen increases in
+# emissions from 1999-2008?
+# Use the ggplot2 plotting system to make a plot answer this question.
+
+baltimore_fips <- "24510"
+
+png(
+  filename = "plot3.png", width = 640, height = 480, units = "px"
+)
+
+pm25_summary_by_year <-
+  filter(pm25, fips == baltimore_fips) %>%
+  group_by(type, year) %>%
+  summarise(total = sum(Emissions))
+
+
+qplot(total,
+      data=pm25_summary_by_year,
+      geom="density",
+      fill=type, alpha=I(.5),
+      main="Baltimore City, Maryland - PM2.5 emissions by year",
+      xlab = "Total emissions by type",
+      ylab="Year"
+)
+
+dev.off()
